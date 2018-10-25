82d8a4690227f0c43992a1b5b39d4523c7d52939
Eufrat Tsaqib <eutsat@gmail.com>
2018-10-26 04:18:46 +0700

    Add dtft and frequency domain

---
+++ freqdom1.m
@@ -0,0 +1,10 @@
+w = [0:1:500]*pi/500; % [0, pi] axis divided into 501 points.
+H = exp(j*w) ./ (exp(j*w) -0.9*ones(1,501));
+magH= abs(H); angH= angle(H);
+subplot(2,1,1); plot(w/pi,magH); grid;
+xlabel('frequency in pi units'); ylabel('|H|');
+title('Magnitude Response');
+subplot(2,1,2); plot(w/pi,angH/pi); grid
+xlabel('frequency in pi units');
+ylabel('Phase in pi Radians');
+title('Phase Response');
